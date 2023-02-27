8ac41e2e00aa256b374cfc999a959702f85c53cc
matthew <permahuman@gmail.com>
2023-02-27 00:19:08 -0800

    Complete refactor of the css animation. All animaiton for the menu popup is now handled by the Web Animation API. Add close button. Menu button opens menu and close menu button closes menu with event listener set on the container.

--- script.js
+++ script.js
@@ -1,23 +1,69 @@
-const menuSlideButton = document.querySelector('.navButton')
+const menuSlider = (menuList) => {
 
-const navMenu = document.querySelector('#navMenu')
-
-menuSlideButton.addEventListener('click', (event) => {
-
-    
-        if(navMenu.style.animationName !== 'slideOut'){
-            navMenu.style.animation = 'slideOut .5s'
-            navMenu.style.display = 'flex'
-
-        }else if(navMenu.style.animationName === 'slideOut'){
-        
-        navMenu.style.animationName = 'slideIn'
-        navMenu.style.animationFillMode = 'forwards'
-        
-        setTimeout(()=>{navMenu.style.display = 'none'},200)
-        
-        //navMenu
+  const sliderForward = () =>{
+  
+    document.querySelector(menuList).animate(
+    [
+      {transform:'scaleY(0)', transformOrigin:'0 0'},
+      {transform:'scaleY(1)', transformOrigin:'0 0'}
+      
+    ], {
+      duration: 500,
+      fill:'both',
+      
+      
     }
-        
+      ).play()
+  }
 
+    const sliderBackward = ()=> {
+
+    document.querySelector(menuList).animate(
+      [
+        {transform:'scaleY(0)', transformOrigin:'0 0'},
+        {transform:'scaleY(1)', transformOrigin:'0 0'}
+        
+      ], {
+        duration: 500,
+        fill:'both',
+        
+        
+      }
+        ).reverse()
+
+    }
+
+return{sliderBackward,sliderForward}
+
+  }
+
+
+const navForward = menuSlider('#navMenu').sliderForward
+
+const navBackward = menuSlider('#navMenu').sliderBackward
+
+
+
+
+const buttonMenuListener = document.querySelector('.menuContainer')
+const closeMenuButton = document.querySelector('.closeMenu')
+
+buttonMenuListener.addEventListener('click', (event)=>{
+if(event.target.className === 'navButton'){
+  navForward()
+  closeMenuButton.style.display = 'flex'
+}else if(event.target.className === 'closeMenu') { 
+  closeMenuButton.style.display = 'none'
+  navBackward()}
 })
+
+
+
+
+
+
+
+
+ 
+
+  
