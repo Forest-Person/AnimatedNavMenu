689c3b051c6df39abdebf792b32bfa8b54d1a0b7
matthew <permahuman@gmail.com>
2023-02-27 21:05:15 -0800

    Create style for the close menu button position absolute and add both buttons to div with flex and row direction. Buttons group together and do not move text below. Disable menu button on click. Enable close button. On click disable close button and enbale menu button again.

--- script.js
+++ script.js
@@ -45,18 +45,30 @@ const navBackward = menuSlider('#navMenu').sliderBackward
 
 
 
+
 const buttonMenuListener = document.querySelector('.menuContainer')
+const navButton = document.querySelector('.navButton')
 const closeMenuButton = document.querySelector('.closeMenu')
 const navMenu = document.querySelector('#navMenu')
 
 buttonMenuListener.addEventListener('click', (event)=>{
 if(event.target.className === 'navButton'){
   navForward()
+  navButton.disabled = true
+  closeMenuButton.disabled = false
   navMenu.style.display = 'flex'
   closeMenuButton.style.display = 'flex'
+  
+  
+  
 }else if(event.target.className === 'closeMenu') { 
+  navBackward()
+  closeMenuButton.disabled = true
+  navButton.disabled = false
   closeMenuButton.style.display = 'none'
-  navBackward()}
+  
+
+}
 })
 
 
